6c459188830642567e3d3f93ce28834fa173dc6b
jeremy perez <glyde06@hotmail.com>
2018-02-15 23:09:25 -0600

    Add Redirect model unit specs

---
+++ spec/wcc/contentful/redirect_spec.rb
@@ -0,0 +1,102 @@
+# frozen_string_literal: true
+
+RSpec.describe WCC::Contentful::Redirect, type: :model do
+
+  describe '.find_by_slug' do
+    before do
+      VCR.turn_on!
+
+      WCC::Contentful.configure do |config|
+        config.access_token = "<CONTENTFUL_ACCESS_TOKEN>"
+        config.space = "<CONTENTFUL_SPACE_ID>"
+        config.default_locale = "en-US"
+      end
+    end
+    context 'when the Redirect model in Contentful has a slug and url, but no pageReference' do
+      it 'should receive a slug string and return a Redirect object' do
+        VCR.use_cassette('models/wcc_contentful/redirect/has_slug_and_url', record: :none) do
+          response = described_class.find_by_slug('redirect-with-slug-and-url')
+          expect(response.url).to eq('https://survey.watermark.org/survey/4b2018')
+        end
+      end
+      it 'should let you know that the pageReference field is nil' do
+        VCR.use_cassette('models/wcc_contentful/redirect/has_slug_and_url', record: :none) do
+          response = described_class.find_by_slug('redirect-with-slug-and-url')
+          expect(response.pageReference.nil?).to eq(true)
+        end
+      end 
+    end
+
+    context 'when the Redirect model in Contentful has a slug and pageReference, but no url' do
+      it 'should receive a slug string and return a Redirect object' do
+        VCR.use_cassette('models/wcc_contentful/redirect/has_slug_and_page_reference', record: :none) do
+          response = described_class.find_by_slug('redirect-with-slug-and-page-reference')
+          expect(response.pageReference.url).to eq('theporchchristmas')
+        end
+      end
+
+      it 'should let you know that the url field is nil' do
+        VCR.use_cassette('models/wcc_contentful/redirect/has_slug_and_page_reference', record: :none) do
+          response = described_class.find_by_slug('redirect-with-slug-and-page-reference')
+          expect(response.url.nil?).to eq(true)
+        end
+      end      
+    end
+
+    context 'when the Redirect model in Contentful has a slug, but no url nor pageReference' do
+      it 'should receive a slug string and return a Redirect object' do
+        VCR.use_cassette('models/wcc_contentful/redirect/has_slug_only', record: :none) do
+          response = described_class.find_by_slug('redirect-with-slug-only')
+          expect(response.slug).to eq('redirect-with-slug-only')
+        end
+      end
+
+      it 'should let you know that the url field is nil' do
+        VCR.use_cassette('models/wcc_contentful/redirect/has_slug_only', record: :none) do
+          response = described_class.find_by_slug('redirect-with-slug-only')
+          expect(response.url.nil?).to eq(true)
+        end
+      end
+
+      it 'should let you know that the pageReference field is nil' do
+        VCR.use_cassette('models/wcc_contentful/redirect/has_slug_only', record: :none) do
+          response = described_class.find_by_slug('redirect-with-slug-only')
+          expect(response.pageReference.nil?).to eq(true)
+        end
+      end 
+    end
+  end
+
+  describe '#location' do
+    before do
+      VCR.turn_on!
+    end
+
+    context 'when the Redirect model in Contentful has a slug and url, but no pageReference' do
+      it 'should return the url of the Redirect model' do
+        VCR.use_cassette('models/wcc_contentful/redirect/has_slug_and_url', record: :none) do
+          response = described_class.find_by_slug('redirect-with-slug-and-url')
+          expect(response.location).to eq("#{response.url}")
+        end
+      end
+    end
+
+    context 'when the Redirect model in Contentful has a slug and pageReference, but no url' do
+      it 'should return the url of the pageReference' do
+        VCR.use_cassette('models/wcc_contentful/redirect/has_slug_and_page_reference', record: :none) do
+          response = described_class.find_by_slug('redirect-with-slug-and-page-reference')
+          expect(response.location).to eq("/#{response.pageReference.url}")
+        end
+      end
+    end
+
+    context 'when the Redirect model in Contentful has a slug, but no url nor pageReference' do
+      it 'should return nil' do
+        VCR.use_cassette('models/wcc_contentful/redirect/has_slug_only', record: :none) do
+          response = described_class.find_by_slug('redirect-with-slug-only')
+          expect(response.location.nil?).to eq(true)
+        end
+      end
+    end
+  end
+end
