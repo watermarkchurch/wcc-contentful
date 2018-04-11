d3dc905daeca2453631505126f0ccd27e0298d41
Justin Powell <jpowellcs@gmail.com>
2018-04-11 15:00:05 -0500

    add type validation to page sections

--- lib/wcc/contentful/model/page.rb
+++ lib/wcc/contentful/model/page.rb
@@ -4,5 +4,5 @@ class WCC::Contentful::Model::Page < WCC::Contentful::Model
   validate_field :title, :String
   validate_field :slug, :String
   validate_field :subpages, :Array, link_to: %w[page]
-  validate_field :sections, :Array
+  validate_field :sections, :Array, link_to: /^section/
 end
