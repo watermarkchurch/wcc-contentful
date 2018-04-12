af2a4ca693338146778da511098d0686bbc72f1e
jeremy perez <glyde06@hotmail.com>
2018-04-12 11:34:19 -0500

    Add Redirect model class with logic for 'location'

---
+++ lib/wcc/contentful/model/redirect.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+class WCC::Contentful::Model::Redirect < WCC::Contentful::Model
+  def location
+    if !url.nil?
+      url
+    elsif valid_page_reference?(pageReference)
+      "/#{pageReference.url}"
+    end
+  end
+
+  def valid_page_reference?(page_ref)
+    if !page_ref.nil? || !defined?(page_ref.url).nil?
+      true
+    else
+      false
+    end
+  end
+end
