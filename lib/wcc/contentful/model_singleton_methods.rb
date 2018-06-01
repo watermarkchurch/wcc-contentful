9c3bd8377d71387587789bc8373a09fde7755db8
Gordon <gburgett@watermark.org>
2018-06-01 16:12:05 -0500

    Refactor model find signatures

--- lib/wcc/contentful/model_singleton_methods.rb
+++ lib/wcc/contentful/model_singleton_methods.rb
@@ -5,38 +5,41 @@
 # This module is extended by all models and defines singleton
 # methods that are not dynamically generated.
 module WCC::Contentful::ModelSingletonMethods
-  def find(id, context = nil)
-    raw = WCC::Contentful::Model.store.find(id)
+  def store(preview = false)
+    if preview
+      if WCC::Contentful::Model.preview_store.nil?
+        raise ArgumentError,
+          'You must include a contentful preview token in your WCC::Contentful.configure block'
+      end
+      WCC::Contentful::Model.preview_store
+    else
+      WCC::Contentful::Model.store
+    end
+  end
+
+  def find(id, context: nil, preview: false)
+    raw = store(preview).find(id)
     new(raw, context) if raw.present?
   end
 
-  def find_all(filter = nil, context = nil)
+  def find_all(context: nil, preview: false, **filter)
     if filter
       filter.transform_keys! { |k| k.to_s.camelize(:lower) }
       bad_fields = filter.keys.reject { |k| self::FIELDS.include?(k) }
       raise ArgumentError, "These fields do not exist: #{bad_fields}" unless bad_fields.empty?
     end
 
-    query = WCC::Contentful::Model.store.find_all(content_type: content_type)
+    query = store(preview).find_all(content_type: content_type)
     query = query.apply(filter) if filter
     query.map { |r| new(r, context) }
   end
 
-  def find_by(filter, context = nil)
+  def find_by(context: nil, preview: false, **filter)
     filter.transform_keys! { |k| k.to_s.camelize(:lower) }
     bad_fields = filter.keys.reject { |k| self::FIELDS.include?(k) }
     raise ArgumentError, "These fields do not exist: #{bad_fields}" unless bad_fields.empty?
 
-    result =
-      if defined?(context[:preview]) && context[:preview] == true
-        if WCC::Contentful::Model.preview_store.nil?
-          raise ArgumentError,
-            'You must include a contentful preview token in your WCC::Contentful.configure block'
-        end
-        WCC::Contentful::Model.preview_store.find_by(content_type: content_type, filter: filter)
-      else
-        WCC::Contentful::Model.store.find_by(content_type: content_type, filter: filter)
-      end
+    result = store(preview).find_by(content_type: content_type, filter: filter)
 
     new(result, context) if result
   end
