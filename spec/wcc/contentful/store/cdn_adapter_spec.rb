1d9ff418d94cc1228592c0cb534daebe9aa7d525
Gordon <gburgett@watermark.org>
2018-06-04 09:38:28 -0500

    Move examples in CDNAdapter spec to correct spot

--- spec/wcc/contentful/store/cdn_adapter_spec.rb
+++ spec/wcc/contentful/store/cdn_adapter_spec.rb
@@ -216,58 +216,32 @@ RSpec.describe WCC::Contentful::Store::CDNAdapter, :vcr do
       expect(found).to eq('item1')
     end
 
-    it 'recursively resolves links if include > 0' do
+    it 'passes query params thru to client' do
+      entry_stub = { 'sys' => { 'type' => 'Entry' } }
+      expect(adapter.client).to receive(:entries)
+        .with({
+          locale: '*',
+          content_type: 'page',
+          'fields.test.en-US' => 'junk',
+          limit: 2,
+          skip: 10,
+          include: 5
+        })
+        .and_return(double(items: [entry_stub], includes: {}))
+
       # act
-      found = adapter.find_all(content_type: 'page', query: {
-        limit: 5,
-        include: 2
-      })
+      found = adapter.find_by(
+        content_type: 'page',
+        filter: { test: 'junk' },
+        query: {
+          limit: 2,
+          skip: 10,
+          include: 5
+        }
+      )
 
       # assert
-      expect(found.result).to be_a(Enumerator::Lazy)
-      items = found.result.force
-      expect(items.count).to eq(11)
-
-      page5 = items[5]
-      expect(page5.dig('sys', 'id')).to eq('3DvAuJkpBCCos2SiGswWW4')
-
-      # depth 1
-      domain_object = page5.dig('fields', 'domainObject', 'en-US')
-      expect(domain_object.dig('sys', 'type')).to eq('Entry')
-
-      # depth 2
-      thumbnail = domain_object.dig('fields', 'thumbnail', 'en-US')
-      expect(thumbnail.dig('sys', 'type')).to eq('Asset')
-    end
-
-    it 'stops resolving links at include depth' do
-      # act
-      found = adapter.find_all(content_type: 'page', query: {
-        limit: 5,
-        include: 2
-      })
-
-      # assert
-      expect(found.result).to be_a(Enumerator::Lazy)
-      items = found.result.force
-      expect(items.count).to eq(11)
-
-      page5 = items[5]
-      expect(page5.dig('sys', 'id')).to eq('3DvAuJkpBCCos2SiGswWW4')
-
-      # depth 1
-      sections = page5.dig('fields', 'sections', 'en-US')
-      expect(sections.all? { |s| s.dig('sys', 'type') == 'Entry' }).to be true
-
-      # depth 2
-      section1 = sections[1]
-      message = section1.dig('fields', 'message', 'en-US')
-      expect(message.dig('sys', 'type')).to eq('Entry')
-
-      # depth 3
-      button = message.dig('fields', 'actionButtons', 'en-US', 0)
-      expect(button.dig('sys', 'type')).to eq('Link')
-      expect(button.dig('sys', 'linkType')).to eq('Entry')
+      expect(found).to equal(entry_stub)
     end
   end
 
@@ -325,29 +299,58 @@ RSpec.describe WCC::Contentful::Store::CDNAdapter, :vcr do
       expect(page.dig('fields', 'title', 'en-US')).to eq('Conferences')
     end
 
-    it 'passes query params thru to client' do
-      expect(adapter.client).to receive(:entries)
-        .with({
-          locale: '*',
-          content_type: 'page',
-          limit: 2,
-          skip: 10,
-          include: 5
-        })
-        .and_return(double(count: 17))
-
+    it 'recursively resolves links if include > 0' do
       # act
-      count = adapter.find_all(
-        content_type: 'page',
-        query: {
-          limit: 2,
-          skip: 10,
-          include: 5
-        }
-      ).count
+      found = adapter.find_all(content_type: 'page', query: {
+        limit: 5,
+        include: 2
+      })
 
       # assert
-      expect(count).to eq(17)
+      expect(found.result).to be_a(Enumerator::Lazy)
+      items = found.result.force
+      expect(items.count).to eq(11)
+
+      page5 = items[5]
+      expect(page5.dig('sys', 'id')).to eq('3DvAuJkpBCCos2SiGswWW4')
+
+      # depth 1
+      domain_object = page5.dig('fields', 'domainObject', 'en-US')
+      expect(domain_object.dig('sys', 'type')).to eq('Entry')
+
+      # depth 2
+      thumbnail = domain_object.dig('fields', 'thumbnail', 'en-US')
+      expect(thumbnail.dig('sys', 'type')).to eq('Asset')
+    end
+
+    it 'stops resolving links at include depth' do
+      # act
+      found = adapter.find_all(content_type: 'page', query: {
+        limit: 5,
+        include: 2
+      })
+
+      # assert
+      expect(found.result).to be_a(Enumerator::Lazy)
+      items = found.result.force
+      expect(items.count).to eq(11)
+
+      page5 = items[5]
+      expect(page5.dig('sys', 'id')).to eq('3DvAuJkpBCCos2SiGswWW4')
+
+      # depth 1
+      sections = page5.dig('fields', 'sections', 'en-US')
+      expect(sections.all? { |s| s.dig('sys', 'type') == 'Entry' }).to be true
+
+      # depth 2
+      section1 = sections[1]
+      message = section1.dig('fields', 'message', 'en-US')
+      expect(message.dig('sys', 'type')).to eq('Entry')
+
+      # depth 3
+      button = message.dig('fields', 'actionButtons', 'en-US', 0)
+      expect(button.dig('sys', 'type')).to eq('Link')
+      expect(button.dig('sys', 'linkType')).to eq('Entry')
     end
   end
 
