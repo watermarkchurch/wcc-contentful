b826fdf274be50e1c4553e90361ccdcc5014b55e
Gordon <gburgett@watermark.org>
2018-06-11 11:27:07 -0500

    Add some doc for model_methods

--- lib/wcc/contentful/model_methods.rb
+++ lib/wcc/contentful/model_methods.rb
@@ -4,6 +4,16 @@
 # This module is included by all models and defines instance
 # methods that are not dynamically generated.
 module WCC::Contentful::ModelMethods
+  ##
+  # Resolves all links in an entry to the specified depth.
+  #
+  # Each link in the entry is recursively retrieved from the store until the given
+  # depth is satisfied.  Depth resolution is unlimited, circular references will
+  # be resolved to the same object.
+  #
+  # * :depth - how far to recursively resolve.  Must be >= 1
+  # * :fields - (optional) A subset of fields whose links should be resolved.  Defaults to all fields.
+  # * :context - passed to the resolved model's `new` function to provide ex. current locale.  See Model#find
   def resolve(depth: 1, fields: nil, context: {})
     raise ArgumentError, "Depth must be > 0 (was #{depth})" unless depth && depth > 0
 
@@ -17,6 +27,14 @@ module WCC::Contentful::ModelMethods
     self
   end
 
+  ##
+  # Turns the current model into a hash representation as though it had been retrieved from
+  # the Contentful API.
+  #
+  # This differs from `#raw` in that it recursively includes the `#to_h`
+  # of resolved links.  It also sets the fields to the value for the entry's `#sys.locale`,
+  # as though the entry had been retrieved from the API with `locale={#sys.locale}` rather
+  # than `locale=*`.
   def to_h(stack = nil)
     raise WCC::Contentful::CircularReferenceError, stack.join(' -> ') if stack&.include?(id)
     stack = [*stack, id]
@@ -36,10 +54,15 @@ module WCC::Contentful::ModelMethods
     }
   end
 
+  ##
+  # Dumps the object to a JSON string using the same resolution method as the above `#to_h`
   def to_json
     to_h.to_json
   end
 
+  ##
+  # Duplicates the raw hash that produced this entry, optionally overwriting fields
+  # with the given field values.
   def raw_dup(fields = nil)
     new_raw = raw.deep_dup
     fields&.each do |(k, v)|
@@ -51,6 +74,16 @@ module WCC::Contentful::ModelMethods
     new_raw
   end
 
+  ##
+  # Duplicates the entry by duplicating the underlying raw structure, optionally
+  # overwriting fields with the given new values.
+  #
+  # Fields must be provided as a hash, as in the following example:
+  #
+  #    foo.dup(
+  #      title: 'New Title',
+  #      some_link: another_model
+  #    )
   def dup(fields = nil)
     self.class.new(raw_dup(fields))
   end
