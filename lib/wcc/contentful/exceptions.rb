30e8044a28cd75345177fbec77946e06f90374dd
Gordon <gburgett@watermark.org>
2018-06-25 10:33:03 -0500

    Add a bit of doc on exceptions

--- lib/wcc/contentful/exceptions.rb
+++ lib/wcc/contentful/exceptions.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 module WCC::Contentful
+  # Raised by {WCC::Contentful.validate_models!} if a content type in the space
+  # does not match the validation defined on the associated model.
   class ValidationError < StandardError
     Message =
       Struct.new(:path, :error) do
@@ -35,12 +37,18 @@ module WCC::Contentful
   class SyncError < StandardError
   end
 
+  # Raised when a constant under {WCC::Contentful::Model} does not match to a
+  # content type in the configured Contentful space
   class ContentTypeNotFoundError < NameError
   end
 
+  # Raised when an entry contains a circular reference and cannot be represented
+  # as a flat tree.
   class CircularReferenceError < StandardError
   end
 
+  # Raised by {WCC::Contentful::ModelMethods#resolve Model#resolve} when attempting
+  # to resolve an entry's links and that entry cannot be found in the space.
   class ResolveError < StandardError
   end
 end
