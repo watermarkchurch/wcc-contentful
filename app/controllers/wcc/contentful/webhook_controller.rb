edfa6d9eae909cfa188e164eac2446df22c323e5
Gordon <gburgett@watermark.org>
2018-07-11 13:22:05 +0200

    Register Contentful mime types as aliases
      Don't overwrite the :json format

--- app/controllers/wcc/contentful/webhook_controller.rb
+++ app/controllers/wcc/contentful/webhook_controller.rb
@@ -50,7 +50,9 @@ module WCC::Contentful
         end
       end
 
-      return if request.content_type == 'application/vnd.contentful.management.v1+json'
+      # 'application/vnd.contentful.management.v1+json' is an alias for the 'application/json'
+      # content-type, so 'request.content_type' will give 'application/json'
+      return if request.headers['Content-Type'] == 'application/vnd.contentful.management.v1+json'
       render json: { msg: 'This endpoint only responds to webhooks from Contentful' }, status: 406
     end
   end
--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -1,3 +1,12 @@
 # frozen_string_literal: true
 
-Mime::Type.register 'application/vnd.contentful.management.v1+json', :json
+# https://www.contentful.com/developers/docs/references/content-management-api/
+Mime::Type.register 'application/vnd.contentful.management.v1+json', :json_mgmt
+# https://www.contentful.com/developers/docs/references/content-delivery-api/
+Mime::Type.register 'application/vnd.contentful.delivery.v1+json',   :json_cda
+
+Mime::Type.register 'application/json', :json, [
+  'application/vnd.contentful.management.v1+json',
+  'application/vnd.contentful.delivery.v1+json',
+  'application/json'
+]
