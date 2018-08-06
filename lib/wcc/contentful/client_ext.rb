d2212b7b35f70f5f5773377c76d1e74663e2ed8c
Reid Cooper <reid.cooper8@gmail.com>
2018-08-06 10:56:07 -0400

    Client Ext now lazily executes either old/new update

--- lib/wcc/contentful/client_ext.rb
+++ lib/wcc/contentful/client_ext.rb
@@ -5,11 +5,12 @@ class Contentful::Client
     alias_method :old_get_http, :get_http
   end
 
+  def self.adapter
+    @adapter ||= WCC::Contentful::SimpleClient.load_adapter(WCC::Contentful.configuration.http_adapter) ||
+      ->(url, query, headers, proxy) { old_get_http(url, query, headers, proxy) }
+  end
+
   def self.get_http(url, query, headers = {}, proxy = {})
-    if override = WCC::Contentful::SimpleClient.load_adapter(WCC::Contentful.configuration.http_adapter)
-      override.call(url, query, headers, proxy)
-    else
-      old_get_http(url, query, headers, proxy)
-    end
+    adapter.call(url, query, headers, proxy)
   end
 end
