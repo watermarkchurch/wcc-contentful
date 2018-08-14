130c4b284aeaa3d8c04fa6c0993e21ea38aa0987
Gordon <gburgett@watermark.org>
2018-08-14 13:26:16 -0500

    Rewrite URLs to point to configured environment

--- lib/wcc/contentful/client_ext.rb
+++ lib/wcc/contentful/client_ext.rb
@@ -12,6 +12,16 @@ class Contentful::Client
   end
 
   def self.get_http(url, query, headers = {}, proxy = {})
+    if environment = WCC::Contentful.configuration.environment
+      url = rewrite_to_environment(url, environment)
+    end
+
     adapter.call(url, query, headers, proxy)
+  end
+
+  REWRITE_REGEXP = /^(https?\:\/\/(?:\w+)\.contentful\.com\/spaces\/[^\/]+\/)(?!environments)(.+)$/
+  def self.rewrite_to_environment(url, environment)
+    return url unless m = REWRITE_REGEXP.match(url)
+    File.join(m[1], 'environments', environment, m[2])
   end
 end
