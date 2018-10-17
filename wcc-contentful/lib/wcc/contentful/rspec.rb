c5b76c111d9b8a5cea261b749f6ef663793acf04
Gordon <gburgett@watermark.org>
2018-10-17 13:30:21 -0500

    Add more doc & ensure wcc/contentful is required

--- wcc-contentful/lib/wcc/contentful/rspec.rb
+++ wcc-contentful/lib/wcc/contentful/rspec.rb
@@ -1,11 +1,17 @@
 # frozen_string_literal: true
 
+require 'wcc/contentful'
+
 require_relative './test'
 
 module WCC::Contentful::RSpec
   include WCC::Contentful::Test::Double
   include WCC::Contentful::Test::Factory
 
+  ##
+  # Builds out a fake Contentful entry for the given content type, and then
+  # stubs the Model API to return that content type for `.find` and `.find_by`
+  # query methods.
   def contentful_stub(content_type, **attrs)
     const = WCC::Contentful::Model.resolve_constant(content_type.to_s)
     instance = contentful_create(content_type, **attrs)
@@ -28,6 +34,8 @@ module WCC::Contentful::RSpec
   end
 end
 
-RSpec.configure do |config|
-  config.include WCC::Contentful::RSpec
+if defined?(RSpec)
+  RSpec.configure do |config|
+    config.include WCC::Contentful::RSpec
+  end
 end
