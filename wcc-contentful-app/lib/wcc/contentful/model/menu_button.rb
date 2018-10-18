dde210af0ba8e62a3a77ad717b8b3b4091b9b292
jeremy perez <glyde06@hotmail.com>
2018-10-18 12:03:21 -0500

    Update menu_button to match the one being used in thePorch

--- wcc-contentful-app/lib/wcc/contentful/model/menu_button.rb
+++ wcc-contentful-app/lib/wcc/contentful/model/menu_button.rb
@@ -6,11 +6,28 @@ class WCC::Contentful::Model::MenuButton < WCC::Contentful::Model
   validate_field :external_link, :String, :optional
   validate_field :link, :Link, :optional, link_to: 'page'
 
+  def external_uri
+    @external_url ||= URI(external_link) if external_link.present?
+  end
+
+  # A menu link is external if `external_link` is present and not relative.
+  def external?
+    external_uri&.scheme.present?
+  end
+
   # Gets either the external link or the slug from the referenced page.
   # Example usage: `<%= link_to button.title, button.href %>`
   def href
     return external_link if external_link
+    url = (link&.try(:slug) || link&.try(:url))
+    return url unless fragment.present?
+    url = URI(url || '')
+    url.fragment = fragment
+    url.to_s
+  end
 
-    link&.try(:slug) || link&.try(:url)
+  def fragment
+    return unless (section = section_link&.try(:bookmark_title) || section_link&.try(:title)).present?
+    CGI.escape(section.gsub(/\W+/,'-'))
   end
 end
