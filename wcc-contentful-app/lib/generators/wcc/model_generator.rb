e15b51c6b2f1e46e3a1c2d5838504505e5bc0177
Gordon <gburgett@watermark.org>
2018-10-23 15:47:41 -0500

    Add section-block-texts to gem

--- wcc-contentful-app/lib/generators/wcc/model_generator.rb
+++ wcc-contentful-app/lib/generators/wcc/model_generator.rb
@@ -5,7 +5,11 @@ module Wcc
     source_root File.expand_path('templates', __dir__)
     argument :model, type: :string
 
-    VALID_MODELS = %w[menu page].freeze
+    VALID_MODELS = %w[
+      menu
+      page
+      section-block-text
+    ].freeze
 
     def initialize(*)
       super
