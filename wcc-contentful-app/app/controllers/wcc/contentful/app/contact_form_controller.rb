c879c40c7c8aeb45ef156494edc12eac42406abc
jeremy perez <glyde06@hotmail.com>
2018-10-26 18:30:59 -0500

    Add contact_form controller/view/routes
    Decided to rename this from 'contact_us' to 'contact_form' due to conflicts
    when dealing with the singular vs plural forms of 'contact_us'. Also, right
    now it seems as though I can't get an app to use the rails route helper methods
    like _url or _path unless I call those helper methods combined with these other
    helper methods you get whenever you mount an engine, so you end up having to
    do 'wcc_contentful_app.contact_form_path' and then in the actuall application, you
    have to start doing things like 'main_app.root_path' wherever the engine routes
    and main app routes are both used. I don't know if this is just the way it has to
    be, but I'm thinking there's probably a better way, I just haven't seen it yet.

---
+++ wcc-contentful-app/app/controllers/wcc/contentful/app/contact_form_controller.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+class WCC::Contentful::App::ContactFormController < ApplicationController
+  def create
+    id = params['id']
+    raise ArgumentError, 'missing form ID' unless id
+    form_model = WCC::Contentful::Model.find(id)
+    to_email = form_model.notificationEmail
+    data = {}
+
+    form_model.fields.each do |item|
+      data[item.title] = params[item.title]
+    end
+
+    UserMailer.contact_us_email(to_email, data).deliver_later
+
+    render json: { type: 'success', message: "Thanks for reaching out. We'll be in touch soon!" }
+  end
+end
--- wcc-contentful-app/config/routes.rb
+++ wcc-contentful-app/config/routes.rb
@@ -2,4 +2,5 @@
 
 WCC::Contentful::App::Engine.routes.draw do
   get '/:slug', to: 'pages#show'
+  post '/contact_form', to: 'contact_form#create'
 end
