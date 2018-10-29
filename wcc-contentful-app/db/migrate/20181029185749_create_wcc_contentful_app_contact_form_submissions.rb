721388d25b325f2556d7960e9057c6006350660c
jeremy perez <glyde06@hotmail.com>
2018-10-29 14:09:31 -0500

    Add migration for persisting contact_form submissions
    To generate this migration from the engine into your main application run the
    following command from the main app:
    rails wcc_contentful_app:install:migrations

---
+++ wcc-contentful-app/db/migrate/20181029185749_create_wcc_contentful_app_contact_form_submissions.rb
@@ -0,0 +1,12 @@
+class CreateWccContentfulAppContactFormSubmissions < ActiveRecord::Migration[5.2]
+  def change
+    create_table :wcc_contentful_app_contact_form_submissions do |t|
+      t.string :full_name
+      t.string :email
+      t.string :phone_number
+      t.text :question
+
+      t.timestamps
+    end
+  end
+end
