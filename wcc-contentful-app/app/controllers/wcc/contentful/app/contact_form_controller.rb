0f0c5ef8c6a0437a38b6835b319d2b1ef535ec24
jeremy perez <glyde06@hotmail.com>
2018-10-31 14:39:47 -0500

    Extract controller logic into section_contact_form model

--- wcc-contentful-app/app/controllers/wcc/contentful/app/contact_form_controller.rb
+++ wcc-contentful-app/app/controllers/wcc/contentful/app/contact_form_controller.rb
@@ -3,24 +3,15 @@
 class WCC::Contentful::App::ContactFormController < ApplicationController
   def create
     id = params['id']
-    raise ArgumentError, 'missing form ID' unless id
+    raise ArgumentError, 'missing form ID' unless params['id']
 
     form_model = WCC::Contentful::Model::SectionContactForm.find(id)
-    to_email = form_model.notificationEmail
     data = params.slice(*form_model.fields.map(&:title))
 
-    UserMailer.contact_form_email(to_email, data).deliver_later
-
-    if WCC::Contentful::App.db_connected?
-      if ActiveRecord::Base.connection.table_exists? 'wcc_contentful_app_contact_form_submissions'
-        WCC::Contentful::App::ContactFormSubmission.create!(
-          full_name: data['First and Last Name'],
-          email: data['Email'],
-          phone_number: data['Phone Number'],
-          question: data['Question']
-        )
-      end
-    end
+    WCC::Contentful::Model::SectionContactForm.send_email(
+      form_model.notificationEmail,
+      data
+    )
 
     render json: { type: 'success', message: "Thanks for reaching out. We'll be in touch soon!" }
   end
--- wcc-contentful-app/lib/wcc/contentful/model/section_contact_form.rb
+++ wcc-contentful-app/lib/wcc/contentful/model/section_contact_form.rb
@@ -1,7 +1,27 @@
 # frozen_string_literal: true
 
 class WCC::Contentful::Model::SectionContactForm < WCC::Contentful::Model
+  def send_email(to_email, data)
+    UserMailer.contact_form_email(to_email, data).deliver_later
+
+    save_contact_form(data)
+  end
+
   def page
     WCC::Contentful::Model::Page.find_by(sections: { id: id })
+  end
+
+  private
+
+  def save_contact_form(data)
+    return unless WCC::Contentful::App.db_connected?
+    return unless ActiveRecord::Base.connection.table_exists? 'wcc_contentful_app_contact_form_submissions'
+
+    WCC::Contentful::App::ContactFormSubmission.create!(
+      full_name: data['First and Last Name'],
+      email: data['Email'],
+      phone_number: data['Phone Number'],
+      question: data['Question']
+    )
   end
 end
