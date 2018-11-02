ae52d9c14a0ec6321e0b1b8178cffa3d13cdd274
Justin Powell <jpowellcs@gmail.com>
2018-11-02 13:20:34 -0500

    improve contact form controller

--- wcc-contentful-app/app/controllers/wcc/contentful/app/contact_form_controller.rb
+++ wcc-contentful-app/app/controllers/wcc/contentful/app/contact_form_controller.rb
@@ -2,13 +2,20 @@
 
 class WCC::Contentful::App::ContactFormController < ApplicationController
   def create
-    id = params['id']
-    raise ArgumentError, 'missing form ID' unless params['id']
-
-    form_model = WCC::Contentful::Model::SectionContactForm.find(id)
-
-    form_model.send_email(params.slice(*form_model.fields.map(&:title)))
+    form_model.send_email(form_params)
 
     render json: { type: 'success', message: "Thanks for reaching out. We'll be in touch soon!" }
+  end
+
+  private
+
+  def form_model
+    raise ArgumentError, 'missing form ID' unless params[:id]
+
+    @form_model ||= WCC::Contentful::Model::SectionContactForm.find(params[:id])
+  end
+
+  def form_params
+    params.permit(*form_model.fields.map(&:title))
   end
 end
