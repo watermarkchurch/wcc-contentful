9057026a6b88747e062f9e236fe792bea9c2223d
Justin Powell <jpowellcs@gmail.com>
2018-11-02 17:40:39 -0500

    dont guard for ApplicationMailer

--- wcc-contentful-app/app/mailers/wcc/contentful/app/contact_mailer.rb
+++ wcc-contentful-app/app/mailers/wcc/contentful/app/contact_mailer.rb
@@ -1,13 +1,11 @@
 # frozen_string_literal: true
 
 module WCC::Contentful::App
-  if defined?(::ApplicationMailer)
-    class ContactMailer < ::ApplicationMailer
-      def contact_form_email(to_email, data)
-        @form_data = data
+  class ContactMailer < ::ApplicationMailer
+    def contact_form_email(to_email, data)
+      @form_data = data
 
-        mail(to: to_email, subject: 'Contact Us Form Submission')
-      end
+      mail(to: to_email, subject: 'Contact Us Form Submission')
     end
   end
 end
