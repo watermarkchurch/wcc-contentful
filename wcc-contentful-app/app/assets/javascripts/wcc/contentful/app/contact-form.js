6288c3333230963cb6e660fba41f0fa95a8b8fe0
Justin Powell <jpowellcs@gmail.com>
2018-11-05 13:41:35 -0600

    move handleResponse outside loop

--- wcc-contentful-app/app/assets/javascripts/wcc/contentful/app/contact-form.js
+++ wcc-contentful-app/app/assets/javascripts/wcc/contentful/app/contact-form.js
@@ -3,33 +3,33 @@ if (typeof window.$ == 'undefined' ) {
 }
 
 $(function() {
+  function handleResponse($form, event, status, xhr) {
+    // Handle backwards compat for [rails/jquery]-ujs ajax callbacks
+    var json
+    if (event.detail) {
+      json = JSON.parse(event.detail[2].response)
+      status = event.detail[1]
+    } else {
+      json = xhr.responseJSON
+    }
+
+    if (status == 'OK') {
+      $form.append($('span').text(json.message))
+      $('input:visible, textarea', $form).val('')
+    } else {
+      alert('Sorry, something went wrong.')
+    }
+  }
+
   $('[data-contact-form]').each(function(_, input) {
     var $form = $(input)
 
-    function handleResponse(event, status, xhr) {
-      // Handle backwards compat for [rails/jquery]-ujs ajax callbacks
-      var json
-      if (event.detail) {
-        json = JSON.parse(event.detail[2].response)
-        status = event.detail[1]
-      } else {
-        json = xhr.responseJSON
-      }
-
-      if (status == 'OK') {
-        $form.append($('span').text(json.message))
-        $('input:visible, textarea', $form).val('')
-      } else {
-        alert('Sorry, something went wrong.')
-      }
-    }
-
     $form.on('ajax:success', function(event, data, status, xhr) {
-      handleResponse(event, status, xhr)
+      handleResponse($form, event, status, xhr)
     })
 
     $form.on('ajax:error', function(event, xhr, status) {
-      handleResponse(event, status, xhr)
+      handleResponse($form, event, status, xhr)
     })
   })
 })
