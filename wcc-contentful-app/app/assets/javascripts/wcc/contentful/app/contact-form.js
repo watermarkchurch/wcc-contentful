57d66fe08b75e10901f017313852ba26a43e2348
Justin Powell <jpowellcs@gmail.com>
2018-11-05 09:52:46 -0600

    remove es6 from contact js

--- wcc-contentful-app/app/assets/javascripts/wcc/contentful/app/contact-form.js
+++ wcc-contentful-app/app/assets/javascripts/wcc/contentful/app/contact-form.js
@@ -1,22 +1,22 @@
-$(window).load(function() {
-  $('[data-contact-form]').each((_, input) => {
-    const $form = $(input);
+$(function() {
+  $('[data-contact-form]').each(function(_, input) {
+    var $form = $(input)
 
     function handleResponse(status, responseJSON) {
       if (responseJSON) {
-        alert(responseJSON.message);
+        alert(responseJSON.message)
       } else {
-        alert('Sorry, something went wrong.');
+        alert('Sorry, something went wrong.')
       }
-      $('input:visible, textarea', $form).val('');
+      $('input:visible, textarea', $form).val('')
     }
 
-    $form.on('ajax:success', (event, data, status, xhr) => {
-      handleResponse(status, xhr.responseJSON);
-    });
+    $form.on('ajax:success', function(event, data, status, xhr) {
+      handleResponse(status, xhr.responseJSON)
+    })
 
-    $form.on('ajax:error', (event, xhr, status) => {
-      handleResponse(status, xhr.responseJSON);
-    });
-  });
+    $form.on('ajax:error', function(event, xhr, status) {
+      handleResponse(status, xhr.responseJSON)
+    })
+  })
 })
