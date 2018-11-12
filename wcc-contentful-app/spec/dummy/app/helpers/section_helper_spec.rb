7b2f9755a702048b867876668f71fa98cd7153c9
jeremy perez <glyde06@hotmail.com>
2018-11-12 17:46:43 -0600

    Add spec for #gather_links_with_classes_data method

--- wcc-contentful-app/spec/dummy/app/helpers/section_helper_spec.rb
+++ wcc-contentful-app/spec/dummy/app/helpers/section_helper_spec.rb
@@ -3,9 +3,14 @@
 require 'rails_helper'
 
 RSpec.describe WCC::Contentful::App::SectionHelper, type: :helper do
-  let(:markdown_string_with_links) {
+  let(:markdown_string_with_links_that_have_classes) {
     "Ministry developed by [Awaken](/awaken \"Awaken's Homepage\"){: .button .white} in Dallas, Texas."\
-    " Just relax. [Watermark Community Church](http://www.watermark.org){: .button-medium .green}"
+    " Just relax. [Watermark Community Church](http://www.watermark.org){: .button-medium .green} ok."\
+    " Last line goes here [Test](https://test.com)."
+  }
+
+  let(:markdown_string_with_classless_link) {
+    "Test line goes here [Test](https://test.com)."
   }
 
   let(:markdown_string_without_links) {
@@ -15,13 +20,48 @@ RSpec.describe WCC::Contentful::App::SectionHelper, type: :helper do
   describe '#links_within_markdown' do
     context 'when given markdown that includes links' do
       it 'returns an array that is not empty' do
-        markdown_links = helper.links_within_markdown(markdown_string_with_links)
+        markdown_links = helper.links_within_markdown(markdown_string_with_links_that_have_classes)
 
         expect(markdown_links).not_to be_empty
       end
       it 'returns an array of all the links in that markdown' do
-        markdown_links = helper.links_within_markdown(markdown_string_with_links)
+        markdown_links = helper.links_within_markdown(markdown_string_with_links_that_have_classes)
         expected_array =
+          [
+            [
+              '[Awaken](/awaken "Awaken\'s Homepage"){: .button .white}',
+              'Awaken',
+              '/awaken "Awaken\'s Homepage"',
+              '{: .button .white}'
+            ],
+            [
+              '[Watermark Community Church](http://www.watermark.org){: .button-medium .green}',
+              'Watermark Community Church',
+              'http://www.watermark.org',
+              '{: .button-medium .green}'
+            ],
+            [
+              '[Test](https://test.com)',
+              'Test',
+              'https://test.com',
+              nil
+            ]
+          ]
+        expect(markdown_links).to match_array(expected_array)
+      end
+    end
+
+    context 'when given markdown that doesn\'t include links' do
+      it 'returns an empty array' do
+        expect(helper.links_within_markdown(markdown_string_without_links)).to be_empty
+      end
+    end
+  end
+
+  describe '#gather_links_with_classes_data' do
+    context 'when given links that include classes' do
+      it 'returns an array that is not empty' do
+        links_with_classes_arr =
           [
             [
               '[Awaken](/awaken "Awaken\'s Homepage"){: .button .white}',
@@ -36,13 +76,26 @@ RSpec.describe WCC::Contentful::App::SectionHelper, type: :helper do
               '{: .button-medium .green}'
             ]
           ]
-        expect(markdown_links).to match_array(expected_array)
+        links_with_classes, raw_classes = helper.gather_links_with_classes_data(links_with_classes_arr)
+
+        expect(links_with_classes).not_to be_empty
       end
     end
 
-    context 'when given markdown that doesn\'t include links' do
+    context 'when given links that don\'t include classes' do
       it 'returns an empty array' do
-        expect(helper.links_within_markdown(markdown_string_without_links)).to be_empty
+        links_without_classes_arr =
+          [
+            [
+              '[Test](https://test.com)',
+              'Test',
+              'https://test.com',
+              nil
+            ]
+          ]
+        links_with_classes, raw_classes = helper.gather_links_with_classes_data(links_without_classes_arr)
+
+        expect(links_with_classes).to be_empty
       end
     end
   end
