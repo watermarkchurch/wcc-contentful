48cc78e4169e436d4cdcc7f45c768f2c703df435
jeremy perez <glyde06@hotmail.com>
2018-11-13 14:40:50 -0600

    Remove comments now that I have tests as documentation

--- wcc-contentful-app/app/helpers/wcc/contentful/app/section_helper.rb
+++ wcc-contentful-app/app/helpers/wcc/contentful/app/section_helper.rb
@@ -63,25 +63,10 @@ module WCC::Contentful::App::SectionHelper
   end
 
   def links_within_markdown(text)
-    # Captures all of the links in the markdown text provided
-    # with each matching link being returned as an array like the following:
-    # [
-    #   '[button text](http://www.watermark.org "Watermark Church"){: .button .white}',
-    #   'button text',
-    #   'http://www.watermark.org "Watermark Church"',
-    #   '{: .button .white}'
-    # ]
     text.scan(/(\[(.*?)\]\((.*?)\)(\{\:.*?\})?)/)
   end
 
   def gather_links_with_classes_data(markdown_links)
-    # If the markdown has links in it then we will iterate over
-    # those links. Each link is an array of 4 values. The last value
-    # in one of the link arrays is where we store the classes for that
-    # link, if it has any. And if it does, we will store the details
-    # of that link in the 'links_with_classes_arr' and then use it
-    # later in the CustomMarkdownRender to actually add the classes
-    # to the link as we're building it.
     links_with_classes_arr = []
     raw_classes_arr = []
     return [links_with_classes_arr, raw_classes_arr] unless markdown_links.present?
@@ -103,13 +88,10 @@ module WCC::Contentful::App::SectionHelper
   end
 
   def remove_markdown_href_class_syntax(raw_classes, text)
-    # remove all of the '{: .button}' syntax from the markdown text
-    # so that it doesn't get rendered to the page
     raw_classes.each { |klass| text.slice!(klass) }
   end
 
   def url_and_title(markdown_link_and_title)
-    # match markdown styled absolute or relative url and title if provided
     match = markdown_link_and_title.scan(/(\s|^)(https?:\/\/\S*|^\/\S*\/*\S*)(?=\s|$)|(\".*?\")/)
     url = match[0][1]
     title = match[1] ? match[1][2] : nil
@@ -117,17 +99,10 @@ module WCC::Contentful::App::SectionHelper
   end
 
   def capture_individual_classes(classes)
-    # receives the '{: .button .white}' class syntax
-    # then returns the classes as an array
-    # ['.button', '.white']
     classes.scan(/[.][\S]*[^\}\s]/)
   end
 
   def combine_individual_classes_to_one_string(classes)
-    # converts an array of classes
-    # ['.button', '.white']
-    # into one string to be used as the class for a url tag
-    # 'button white'
     class_string = ''
     classes.each do |klass|
       class_string += klass.tr('.', '') + ' '
