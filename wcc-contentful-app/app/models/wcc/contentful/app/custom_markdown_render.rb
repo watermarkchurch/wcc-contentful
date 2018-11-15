8da5a531e091c48fc20097532222e8c5640709f2
jeremy perez <glyde06@hotmail.com>
2018-11-15 12:37:27 -0600

    Rubocop changes

--- wcc-contentful-app/app/models/wcc/contentful/app/custom_markdown_render.rb
+++ wcc-contentful-app/app/models/wcc/contentful/app/custom_markdown_render.rb
@@ -11,14 +11,18 @@ module WCC::Contentful::App
       link_with_class_data =
         @options[:links_with_classes].find do |link_with_class|
           link_with_class[0] == link &&
-              link_with_class[2] == CGI.unescape_html(content)
+            link_with_class[2] == CGI.unescape_html(content)
         end
-      
+
       link_class = link_with_class_data ? link_with_class_data[3] : nil
-      ActionController::Base.helpers.link_to(content, link, hyperlink_attributes(title, link_class, link))
+      ActionController::Base.helpers.link_to(
+        content,
+        link,
+        hyperlink_attributes(title, link, link_class)
+      )
     end
 
-    def hyperlink_attributes(title, link_class = nil, url)
+    def hyperlink_attributes(title, url, link_class = nil)
       default_link_attrs =
         use_target_blank?(url) ? @options[:link_attributes] : @options[:link_attributes].except(:target)
       { title: title, class: link_class }.merge(default_link_attrs)
