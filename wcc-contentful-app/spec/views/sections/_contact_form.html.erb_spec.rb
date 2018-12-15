64deac325524ae26b6de74690270c38de2dbf65c
jeremy perez <glyde06@hotmail.com>
2018-12-14 18:09:03 -0600

    Get notification email from Contentful every time for security

--- wcc-contentful-app/spec/views/sections/_contact_form.html.erb_spec.rb
+++ wcc-contentful-app/spec/views/sections/_contact_form.html.erb_spec.rb
@@ -5,37 +5,47 @@ require 'rails_helper'
 RSpec.describe 'sections/contact_form' do
   helper WCC::Contentful::App::SectionHelper
 
-  it 'raises action view template error if text is nil' do
-    section = contentful_create('section-contact-form',
-      text: nil)
+  context 'when partial is rendered from components/section' do
+    it 'raises action view template error if text is nil' do
+      section = contentful_create('section-contact-form',
+        text: nil)
 
-    expect {
-      render 'components/section', section: section
-    }.to raise_error(ActionView::Template::Error)
+      expect {
+        render 'components/section', section: section, email_object_id: 'IDGOESHERE'
+      }.to raise_error(ActionView::Template::Error)
+    end
+
+    it 'processes the markdown in the section' do
+      section = contentful_create('section-contact-form',
+        text: '## This should be an H2')
+
+      render 'components/section', section: section, email_object_id: 'IDGOESHERE'
+
+      expect(rendered).to match(/<h2>This should be an H2<\/h2>/)
+    end
+
+    it 'does NOT accept email_object_id' do
+      email = 'ez.net'
+      section = contentful_create('section-contact-form',
+        text: '## This should be an H2',
+        notification_email: email)
+
+      render 'components/section', section: section, email_object_id: 'IDGOESHERE'
+
+      expect(rendered).to_not have_selector('input#email-object-id', visible: false)
+    end
   end
 
-  it 'processes the markdown in the section' do
-    section = contentful_create('section-contact-form',
-      text: '## This should be an H2')
+  context 'when partial is rendered directly' do
+    it 'accepts and renders email_object_id as a hidden field' do
+      email = 'ez.net'
+      section = contentful_create('section-contact-form',
+        text: '## This should be an H2',
+        notification_email: email)
 
-    render 'components/section', section: section
+      render 'sections/contact_form', section: section, email_object_id: 'IDGOESHERE'
 
-    expect(rendered).to match(/<h2>This should be an H2<\/h2>/)
-  end
-
-  it 'handles person_email' do
-    email = 'ez.net'
-    not_the_email = 'ez.bucketz'
-    section = contentful_create('section-contact-form',
-      text: '## This should be an H2',
-      notification_email: email)
-
-    render 'components/section', section: section
-
-    expect(rendered).to have_selector("input#recipient-email[value='#{email}']", visible: false)
-    expect(rendered).to_not have_selector(
-      "input#recipient-email[value='#{not_the_email}']",
-      visible: false
-    )
+      expect(rendered).to have_selector('input#email-object-id', visible: false)
+    end
   end
 end
