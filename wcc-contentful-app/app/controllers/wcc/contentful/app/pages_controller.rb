375375e570c80a4c344d5a88f22fac95aa69d04b
Justin Powell <jpowellcs@gmail.com>
2019-01-14 16:23:25 -0600

    resolve page & redirect constants before finding

--- wcc-contentful-app/app/controllers/wcc/contentful/app/pages_controller.rb
+++ wcc-contentful-app/app/controllers/wcc/contentful/app/pages_controller.rb
@@ -5,23 +5,31 @@ class WCC::Contentful::App::PagesController < ApplicationController
 
   def index
     @page = global_site_config&.homepage ||
-      WCC::Contentful::Model::Page.find_by(slug: '/', options: { include: 3 })
+      page_model.find_by(slug: '/', options: { include: 3 })
     render 'pages/show'
   end
 
   def show
     slug = '/' + params[:slug]
-    @page = WCC::Contentful::Model::Page.find_by(slug: slug, options: { include: 3 })
+    @page = page_model.find_by(slug: slug, options: { include: 3 })
 
     return render 'pages/show' if @page
 
-    redirect = WCC::Contentful::Model::Redirect.find_by(slug: slug, options: { include: 0 })
+    redirect = redirect_model.find_by(slug: slug, options: { include: 0 })
     raise WCC::Contentful::App::PageNotFoundError, slug unless redirect
 
     redirect_to redirect.href
   end
 
   private
+
+  def page_model
+    WCC::Contentful::Model.resolve_constant('page')
+  end
+
+  def redirect_model
+    WCC::Contentful::Model.resolve_constant('redirect')
+  end
 
   def global_site_config
     # They may have not installed `site-config` in the project
