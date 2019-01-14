1e6a2d900009fddf10f7a05d367c193bf7ae3203
Justin Powell <jpowellcs@gmail.com>
2019-01-14 16:36:25 -0600

    adds test case for overloading Page class

--- wcc-contentful-app/spec/requests/wcc/contentful/app/pages_controller_spec.rb
+++ wcc-contentful-app/spec/requests/wcc/contentful/app/pages_controller_spec.rb
@@ -2,6 +2,9 @@
 
 require 'rails_helper'
 
+class MyPage < WCC::Contentful::Model::Page
+end
+
 RSpec.describe WCC::Contentful::App::PagesController, type: :request do
   it 'loads homepage off of site config' do
     page = contentful_create('page', slug: '/')
@@ -26,6 +29,19 @@ RSpec.describe WCC::Contentful::App::PagesController, type: :request do
     expect(assigns(:page)).to eq(page)
   end
 
+  it 'allows overloading the Page model' do
+    allow(::WCC::Contentful::Model).to receive(:resolve_constant)
+      .with('page')
+      .and_return(MyPage)
+
+    page = contentful_stub('page', slug: '/test')
+
+    expect(::MyPage).to receive(:find_by).with(hash_including(slug: '/test'))
+      .and_return(page)
+
+    get '/test'
+  end
+
   it 'loads page by slug' do
     page = contentful_stub('page', slug: '/test')
 
@@ -35,7 +51,7 @@ RSpec.describe WCC::Contentful::App::PagesController, type: :request do
   end
 
   it 'uses redirect when given' do
-    expect(WCC::Contentful::Model::Page).to receive(:find_by)
+    expect(MyPage).to receive(:find_by)
       .and_return(nil)
     redirect = contentful_stub('redirect',
       slug: '/test',
@@ -47,7 +63,7 @@ RSpec.describe WCC::Contentful::App::PagesController, type: :request do
   end
 
   it 'raises exception when page not found' do
-    expect(WCC::Contentful::Model::Page).to receive(:find_by)
+    expect(MyPage).to receive(:find_by)
       .and_return(nil)
     expect(WCC::Contentful::Model::Redirect).to receive(:find_by)
       .and_return(nil)
