172620d501b36071f6446312beb086f762c701a5
Justin Powell <jpowellcs@gmail.com>
2019-01-16 17:35:19 -0600

    sanitize server message before rendering response

--- wcc-contentful-app/app/assets/javascripts/wcc/contentful/app/contact-form.js
+++ wcc-contentful-app/app/assets/javascripts/wcc/contentful/app/contact-form.js
@@ -4,12 +4,15 @@ if (typeof window.$ == 'undefined' ) {
 
 $(function() {
   function warningAlert(message) {
-    return '<div class="alert alert-danger alert-dismissible fade show">' +
-      message +
+    var div = $('<div>')
+      .addClass('alert alert-danger alert-dismissible fade show')
+      .text(message)
+
+    return div.append(
       '<button type="button" class="close" data-dismiss="alert" aria-label="Close">' +
         '<span aria-hidden="true">&times;</span>' +
-      '</button>' +
-    '</div>'
+      '</button>'
+    )
   }
 
   function handleResponse($form, event, status, xhr) {
