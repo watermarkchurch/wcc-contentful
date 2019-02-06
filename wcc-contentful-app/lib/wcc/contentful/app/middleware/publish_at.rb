b1d992ac69350e708c50b4ed4689665453562648
Gordon <gburgett@watermark.org>
2019-02-06 17:37:22 -0600

    Fix publish_at initialization issue in specs

--- wcc-contentful-app/lib/wcc/contentful/app/middleware/publish_at.rb
+++ wcc-contentful-app/lib/wcc/contentful/app/middleware/publish_at.rb
@@ -6,7 +6,8 @@ module WCC::Contentful::App::Middleware
 
     def self.call(store, content_delivery_params, config)
       # This does not apply in preview mode
-      return if content_delivery_params.find { |array| array[:preview] == true }
+      return if content_delivery_params&.find { |array| array[:preview] == true }
+
       super
     end
 
