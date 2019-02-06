1e7c0c59a1d1a6ef189d66f10ac02a24c4684af0
Gordon <gburgett@watermark.org>
2019-02-06 13:52:02 -0600

    Define schema for lazy cache store spec

--- wcc-contentful/lib/wcc/contentful/store/base.rb
+++ wcc-contentful/lib/wcc/contentful/store/base.rb
@@ -169,7 +169,7 @@ module WCC::Contentful::Store
       def resolve_includes(entry, depth)
         return entry unless entry && depth && depth > 0
 
-        WCC::Contentful::LinkVisitor.new(entry, :Link, depth: depth).map do |val|
+        WCC::Contentful::LinkVisitor.new(entry, :Link, :Asset, depth: depth).map do |val|
           resolve_link(val)
         end
       end
