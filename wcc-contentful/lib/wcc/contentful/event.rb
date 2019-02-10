631417f77903878231a7baecd6956e4287da13f7
Gordon <gburgett@watermark.org>
2019-02-10 11:11:22 -0600

    Use a registry and register on included

--- wcc-contentful/lib/wcc/contentful/event.rb
+++ wcc-contentful/lib/wcc/contentful/event.rb
@@ -1,80 +1,104 @@
 # frozen_string_literal: true
 
-class WCC::Contentful::Event
-  def initialize(raw, context = nil)
-    @raw = raw.freeze
+module WCC::Contentful::Event
+  extend ActiveSupport::Concern
 
-    created_at = raw.dig('sys', 'createdAt')
-    created_at = Time.parse(created_at) if created_at.present?
-    updated_at = raw.dig('sys', 'updatedAt')
-    updated_at = Time.parse(updated_at) if updated_at.present?
-    @sys = WCC::Contentful::Sys.new(
-      raw.dig('sys', 'id'),
-      raw.dig('sys', 'type'),
-      raw.dig('sys', 'locale') || context.try(:[], :locale) || 'en-US',
-      raw.dig('sys', 'space', 'sys', 'id'),
-      created_at,
-      updated_at,
-      raw.dig('sys', 'revision'),
-      OpenStruct.new(context).freeze
-    )
+  def self.from_raw(raw, context = nil)
+    const = Registry.instance.get(raw.dig('sys', 'type'))
+
+    const.new(raw, context)
   end
 
-  attr_reader :sys
-  attr_reader :raw
-  delegate :id, to: :sys
-  delegate :type, to: :sys
-  delegate :created_at, to: :sys
-  delegate :updated_at, to: :sys
-  delegate :revision, to: :sys
-  delegate :space, to: :sys
+  class Registry
+    include Singleton
 
-  delegate :dig, :[], to: :raw
+    def get(name)
+      @event_types ||= {}
+      @event_types[name] || WCC::Contentful::Event::Unknown
+    end
 
-  class << self
-    def from_raw(raw, context = nil)
-      const = EVENT_TYPES[raw.dig('sys', 'type')]
-      const ||= WCC::Contentful::Event::Unknown
+    def register(constant)
+      name = constant.try(:type) || constant.name.demodulize
+      unless constant.respond_to?(:new)
+        raise ArgumentError, "Constant #{constant} does not define 'new'"
+      end
 
-      const.new(raw, context)
+      @event_types ||= {}
+      @event_types[name] = constant
     end
   end
 
-  class Entry < WCC::Contentful::Event
-    def entry
-      @entry ||= WCC::Contentful::Model.new_from_raw(raw, sys.context)
-    end
-  end
+  included do
+    Registry.instance.register(self)
 
-  class Asset < WCC::Contentful::Event
-    def asset
-      @asset ||= WCC::Contentful::Model.new_from_raw(raw, sys.context)
-    end
+    def initialize(raw, context = nil)
+      @raw = raw.freeze
 
-    alias_method :entry, :asset
-  end
-
-  class DeletedEntry < WCC::Contentful::Event
-    def entry
-      @entry ||= WCC::Contentful::Model.new_from_raw(raw, sys.context)
-    end
-  end
-
-  class DeletedAsset < WCC::Contentful::Event
-    def asset
-      @asset ||= WCC::Contentful::Model.new_from_raw(raw, sys.context)
+      created_at = raw.dig('sys', 'createdAt')
+      created_at = Time.parse(created_at) if created_at.present?
+      updated_at = raw.dig('sys', 'updatedAt')
+      updated_at = Time.parse(updated_at) if updated_at.present?
+      @sys = WCC::Contentful::Sys.new(
+        raw.dig('sys', 'id'),
+        raw.dig('sys', 'type'),
+        raw.dig('sys', 'locale') || context.try(:[], :locale) || 'en-US',
+        raw.dig('sys', 'space', 'sys', 'id'),
+        created_at,
+        updated_at,
+        raw.dig('sys', 'revision'),
+        OpenStruct.new(context).freeze
+      )
     end
 
-    alias_method :entry, :asset
+    attr_reader :sys
+    attr_reader :raw
+    delegate :id, to: :sys
+    delegate :type, to: :sys
+    delegate :created_at, to: :sys
+    delegate :updated_at, to: :sys
+    delegate :revision, to: :sys
+    delegate :space, to: :sys
+
+    delegate :dig, :[], to: :raw
+  end
+end
+
+class WCC::Contentful::Event::Entry
+  include WCC::Contentful::Event
+
+  def entry
+    @entry ||= WCC::Contentful::Model.new_from_raw(raw, sys.context)
+  end
+end
+
+class WCC::Contentful::Event::Asset
+  include WCC::Contentful::Event
+
+  def asset
+    @asset ||= WCC::Contentful::Model.new_from_raw(raw, sys.context)
   end
 
-  class Unknown < WCC::Contentful::Event
+  alias_method :entry, :asset
+end
+
+class WCC::Contentful::Event::DeletedEntry
+  include WCC::Contentful::Event
+
+  def entry
+    @entry ||= WCC::Contentful::Model.new_from_raw(raw, sys.context)
+  end
+end
+
+class WCC::Contentful::Event::DeletedAsset
+  include WCC::Contentful::Event
+
+  def asset
+    @asset ||= WCC::Contentful::Model.new_from_raw(raw, sys.context)
   end
 
-  EVENT_TYPES = {
-    'Entry' => WCC::Contentful::Event::Entry,
-    'Asset' => WCC::Contentful::Event::Asset,
-    'DeletedEntry' => WCC::Contentful::Event::DeletedEntry,
-    'DeletedAsset' => WCC::Contentful::Event::DeletedAsset
-  }.freeze
+  alias_method :entry, :asset
+end
+
+class WCC::Contentful::Event::Unknown
+  include WCC::Contentful::Event
 end
