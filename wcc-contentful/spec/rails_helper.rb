778e59a415d3a84f7b01b23a9a1163a1106dd915
Gordon <gburgett@watermark.org>
2020-03-18 15:26:50 -0500

    Can't return at a top level file
      on Travis CI:
        rails_helper.rb:22: Invalid return

--- wcc-contentful/spec/rails_helper.rb
+++ wcc-contentful/spec/rails_helper.rb
@@ -10,50 +10,47 @@ rescue Gem::LoadError => e
   warn "WARNING: Cannot load rails - some tests will be skipped\n#{e}"
 end
 
-unless defined?(Rails)
+if defined?(Rails)
+  ENV['RAILS_ENV'] ||= 'test'
+
+  # require rails libraries:
+  #   railtie includes rails + framework middleware
+  #   active_job for any job specs
+  require 'action_controller/railtie'
+  require 'active_job'
+
+  # require rails specific code
+  require 'wcc/contentful/rails'
+
+  # require dummy rails app for engine related specs
+  VCR.use_cassette('models/wcc_contentful/content_types/init_mgmt_api', record: :none) do
+    require File.expand_path('dummy/config/environment.rb', __dir__)
+  end
+
+  # require rspec-rails to simulate framework behavior in specs
+  require 'rspec/rails'
+
+  ENGINE_RAILS_ROOT = File.join(File.dirname(__FILE__), '../')
+
+  # Hack to debug autoloading issues
+  # https://makandracards.com/makandra/48754-how-to-debug-rails-autoloading
+  ActiveSupport::Dependencies.singleton_class.prepend(Module.new do
+    def load_missing_constant(*args)
+      Rails.logger.debug "#{__method__}(#{args.map(&:inspect).join(', ')})"
+      super
+    end
+
+    def search_for_file(*args)
+      Rails.logger.debug "#{__method__}(#{args.map(&:inspect).join(', ')})"
+      Rails.logger.debug "  searching in paths #{autoload_paths}"
+      super
+    end
+  end)
+else
   RSpec.configure do |c|
     # skip rspec-rails spec types
     c.before(:each, rails: true) do
       skip 'rails is not loaded'
     end
   end
-
-  # rails is not loaded in this test run
-  return
 end
-
-ENV['RAILS_ENV'] ||= 'test'
-
-# require rails libraries:
-#   railtie includes rails + framework middleware
-#   active_job for any job specs
-require 'action_controller/railtie'
-require 'active_job'
-
-# require rails specific code
-require 'wcc/contentful/rails'
-
-# require dummy rails app for engine related specs
-VCR.use_cassette('models/wcc_contentful/content_types/init_mgmt_api', record: :none) do
-  require File.expand_path('dummy/config/environment.rb', __dir__)
-end
-
-# require rspec-rails to simulate framework behavior in specs
-require 'rspec/rails'
-
-ENGINE_RAILS_ROOT = File.join(File.dirname(__FILE__), '../')
-
-# Hack to debug autoloading issues
-# https://makandracards.com/makandra/48754-how-to-debug-rails-autoloading
-ActiveSupport::Dependencies.singleton_class.prepend(Module.new do
-  def load_missing_constant(*args)
-    Rails.logger.debug "#{__method__}(#{args.map(&:inspect).join(', ')})"
-    super
-  end
-
-  def search_for_file(*args)
-    Rails.logger.debug "#{__method__}(#{args.map(&:inspect).join(', ')})"
-    Rails.logger.debug "  searching in paths #{autoload_paths}"
-    super
-  end
-end)
