c18a0916871a12578e5a352a90c7d6785019346e
Gordon <gburgett@watermark.org>
2020-03-27 14:42:08 -0500

    Ensure all async jobs use test adapter

--- wcc-contentful/spec/job_helper.rb
+++ wcc-contentful/spec/job_helper.rb
@@ -12,6 +12,8 @@ end
 
 if defined?(ActiveJob)
   ActiveJob::Base.queue_adapter = :test
+  require 'wcc/contentful/sync_engine'
+  WCC::Contentful::SyncEngine::Job.queue_adapter = :test
 else
   RSpec.configure do |c|
     # skip job specs
