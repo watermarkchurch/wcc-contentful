56cb0187d5fde4cec15ad46e6da659377752ac86
Gordon <gburgett@watermark.org>
2021-10-06 14:54:35 -0500

    Release v1.0.1

--- wcc-contentful-app/lib/wcc/contentful/app/version.rb
+++ wcc-contentful-app/lib/wcc/contentful/app/version.rb
@@ -3,7 +3,7 @@
 module WCC
   module Contentful
     module App
-      VERSION = '1.0.0'
+      VERSION = '1.0.1'
     end
   end
 end
--- wcc-contentful-graphql/lib/wcc/contentful/graphql/version.rb
+++ wcc-contentful-graphql/lib/wcc/contentful/graphql/version.rb
@@ -3,7 +3,7 @@
 module WCC
   module Contentful
     module Graphql
-      VERSION = '1.0.0'
+      VERSION = '1.0.1'
     end
   end
 end
