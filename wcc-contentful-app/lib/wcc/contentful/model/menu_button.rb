7306f015cc78f72d13bd2a7e45271c594faf68e8
Gordon <gburgett@watermark.org>
2023-03-01 15:52:45 -0600

    Allow overriding locale rewrite logic

--- wcc-contentful-app/lib/wcc/contentful/model/menu_button.rb
+++ wcc-contentful-app/lib/wcc/contentful/model/menu_button.rb
@@ -18,12 +18,25 @@ class WCC::Contentful::Model::MenuButton < WCC::Contentful::Model
     url = (link&.try(:slug) || link&.try(:url))
     url = URI(url || '')
 
-    url.path = "/#{sys.locale}#{url.path}" if sys.locale != WCC::Contentful.configuration.default_locale
+    url = rewrite_to_locale(url, sys.locale) if default_locale && sys.locale != default_locale
+
     url.fragment = fragment if fragment.present?
     url.to_s
   end
 
   def fragment
     WCC::Contentful::App::SectionHelper.section_id(section_link) if section_link
+  end
+
+  private
+
+  def default_locale
+    WCC::Contentful.configuration&.default_locale
+  end
+
+  def rewrite_to_locale(uri, locale)
+    uri = URI(uri)
+    uri.path = File.join("/#{locale}", uri.path)
+    uri.to_s
   end
 end
