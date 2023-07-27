ce062ca8665d8f11ac140380cbcacf9780115871
Ryan Gibson <gibsorya@gmail.com>
2023-07-27 15:53:23 -0500

    wcc-contentful-app still needs sprockets for older rails versions

--- wcc-contentful-app/lib/wcc/contentful/app/engine.rb
+++ wcc-contentful-app/lib/wcc/contentful/app/engine.rb
@@ -2,6 +2,14 @@
 
 module WCC::Contentful::App
   class Engine < ::Rails::Engine
+    if config.try(:assets)
+      config.assets.precompile += %w[*.jpg *.png *.svg]
+      config.assets.precompile +=
+        [
+          'config/manifest.js'
+        ].map { |f| File.expand_path("../../../../../app/assets/#{f}", __FILE__) }
+    end
+
     config.generators do |g|
       g.test_framework :rspec, fixture: false
     end
