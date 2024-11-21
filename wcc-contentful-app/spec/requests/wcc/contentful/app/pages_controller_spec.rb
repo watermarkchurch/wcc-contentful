ab257b36dd4965cb304c6d92eecb2ecd94ba850d
Gordon <gburgett@watermark.org>
2024-11-21 12:18:10 -0600

    better assert v7.2 behavior

--- wcc-contentful-app/spec/requests/wcc/contentful/app/pages_controller_spec.rb
+++ wcc-contentful-app/spec/requests/wcc/contentful/app/pages_controller_spec.rb
@@ -75,13 +75,17 @@ RSpec.describe WCC::Contentful::App::PagesController, type: :request do
     expect(WCC::Contentful::Model::Redirect).to receive(:find_by)
       .and_return(nil)
 
+    if Rails.version >= '7.2'
       get '/not-found'
 
-    # Rails 7.2 behavior
-    expect(response).to have_http_status(500)
-  rescue WCC::Contentful::App::PageNotFoundError
-    # Rails 7.1 and below behavior
-    # OK
+      # Rails 7.2 behavior
+      expect(response).to have_http_status(500)
+    else
+      # Rails 7.1 and below behavior
+      expect {
+        get '/not-found'
+      }.to raise_error(WCC::Contentful::App::PageNotFoundError)
+    end
   end
 
   it 'uses preview if preview param set' do
@@ -127,14 +131,18 @@ RSpec.describe WCC::Contentful::App::PagesController, type: :request do
       .with(slug: '/test', options: { include: 0, preview: false })
 
     with_preview_password do |_pw|
-      get '/test', params: { preview: 'some other password' }
-    end
+      if Rails.version >= '7.2'
+        get '/test', params: { preview: 'some other password' }
 
-    # Rails 7.2 behavior
-    expect(response).to have_http_status(500)
-  rescue WCC::Contentful::App::PageNotFoundError
-    # Rails 7.1 and below behavior
-    # OK
+        # Rails 7.2 behavior
+        expect(response).to have_http_status(500)
+      else
+        # Rails 7.1 and below behavior
+        expect {
+          get '/test', params: { preview: 'some other password' }
+        }.to raise_error(WCC::Contentful::App::PageNotFoundError)
+      end
+    end
   end
 
   it 'uses application controller defined preview? function' do
@@ -165,14 +173,18 @@ RSpec.describe WCC::Contentful::App::PagesController, type: :request do
 
     # act
     with_preview_password do |_pw|
-      get '/test', params: { preview: 'some other password' }
-    end
+      if Rails.version >= '7.2'
+        get '/test', params: { preview: 'some other password' }
 
-    # Rails 7.2 behavior
-    expect(response).to have_http_status(500)
-  rescue WCC::Contentful::App::PageNotFoundError
-    # Rails 7.1 and below behavior
-    # OK
+        # Rails 7.2 behavior
+        expect(response).to have_http_status(500)
+      else
+        # Rails 7.1 and below behavior
+        expect {
+          get '/test', params: { preview: 'some other password' }
+        }.to raise_error(WCC::Contentful::App::PageNotFoundError)
+      end
+    end
   end
 
   def with_preview_password
